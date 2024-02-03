488a0cccb691777b0359907319ae037dd31a27e8
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-02-03 03:00:14 +0300

    Add the routes to use with the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'root#index'
+  namespace :api do
+    namespace :v1 do
+      get 'messages/index', to: 'messages#index'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
