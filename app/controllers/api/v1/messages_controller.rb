1061c0697a923f84725ab4cd3f8c2dd6a9f8721c
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-02-03 03:48:56 +0300

    Fix the linter errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MessagesController < ApplicationController
-    def index
-      @random_greeting = Message.order('RANDOM()').first
-  
-      if @random_greeting
-        render json: { greeting: @random_greeting.message }
-      else
-        render json: { greeting: 'No greetings found' }
-      end
+  def index
+    @random_greeting = Message.order('RANDOM()').first
+
+    if @random_greeting
+      render json: { greeting: @random_greeting.message }
+    else
+      render json: { greeting: 'No greetings found' }
     end
   end
+end
